cb15c109bfc6cfe43f6db6b2e77cbe6eca99cd9a
Ace-Phatcharapol <Phatcharapol2@gmail.com>
2024-06-21 15:34:04 +0700

    Update Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { faYoutube,faFacebookF,faTwitter,faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +17,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   standalone: true,
   imports: [RouterLink, RouterOutlet, FontAwesomeModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  faYoutube = faYoutube;
+  faFacebookF = faFacebookF;
+  faTwitter = faTwitter;
+  faGithub = faGithub;
+}
